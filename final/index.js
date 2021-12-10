c8720163445573dc4ac4908b4a662138185c6039
esmee <esna9705@colorado.edu>
2021-12-10 14:33:02 -0700

    final project

---
+++ final/index.js
@@ -0,0 +1,11 @@
+console.log('hello!');
+
+let pics = 4;
+let pic;
+
+for(i=0;i<pics;i++){
+  pic = 1+i;
+}
+  document.getElementById("myButton").onclick = function () {
+          location.href = "products.html";
+}
